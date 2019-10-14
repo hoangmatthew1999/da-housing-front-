57d7921844c1cf0643b9e0ced343b7827b7757d8
Matthew Hoang <hoangmatthew1999@gmail.com>
2019-10-13 20:03:55 -0700

    started working on the home page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Flexbox from 'flexbox-react';
+import 'react-bootstrap';
 
+const styles = {
+  card:{width: "18rem"} 
+}
+
+function Cards(){
+  return (
+    <div className="card" style = {styles.card}>
+      <img src="..." class="card-img-top" />
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" className="btn btn-primary">Go somewhere</a>
+          </div>    
+      </div>
+
+
+  )
+}
+function Navigation (){
+  const styles = {width: "50em",
+    padding: 0,
+  };
+  return(
+    <div>
+      <NavBar>
+      <Navbar.Brand href="#home">Home</Navbar.Brand>
+      </NavBar>
+    </div>
+  )
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NavBar/>
+      <Cards/>
+      <Cards/>
     </div>
+
   );
 }
 
