5f34470840b14b6d558d4af3222a3bded2b35b78
Matthew Hoang <hoangmatthew1999@gmail.com>
2019-10-26 13:22:41 -0700

    added react router to the navigation

--- src/App.js
+++ src/App.js
@@ -3,12 +3,28 @@ import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Flexbox from 'flexbox-react';
-import {Navbar, Nav} from 'react-bootstrap';
+import {Navbar, Nav, Form} from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 const styles = {
   card:{width: "18rem"} 
 }
 
+function SignIn(){
+  return(
+  <div>
+      Username:<input type = "text"/>
+      Password: <input type = "password"/>
+
+  </div>
+  )
+}
 function Cards(){
   return (
     <div className="card" style = {styles.card}>
@@ -31,8 +47,19 @@ function Navigation (){
   <Navbar.Toggle aria-controls="basic-navbar-nav" />
   <Navbar.Collapse id="basic-navbar-nav">
     <Nav className="mr-auto">
-      <Nav.Link href="#home">Home</Nav.Link>
-      <Nav.Link href="#link">Link</Nav.Link>
+
+      <Router>
+      <Link to ="/"><Nav.Link href="#home" >Home</Nav.Link></Link>
+      <Link to = "/Link"><Nav.Link href="#link">Link</Nav.Link></Link> 
+      <Link to = "/Sign-In"><Nav.Link href="#Sign In">Sign In</Nav.Link></Link>
+
+      <Switch>
+          <Route path="/Sign-In">
+            <SignIn />
+          </Route>
+
+      </Switch>
+      </Router>
     </Nav>
   </Navbar.Collapse>
 </Navbar>
@@ -44,10 +71,10 @@ function App() {
     <div>
       <Navigation/>
       <Cards/>
-      <Cards/>
     </div>
 
   );
 }
+
 
 export default App;
---
+++ src/SignIn.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function SignIn(){
+    return(
+    <div>
+        Username:<input type = "text"/>
+        Password: <input type = "password"/>
+  
+    </div>
+    )
+  }
